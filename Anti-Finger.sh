55dedf1d82ffc67dd36cedda839da9eadf28fce6
Tayakorn000 <154537903+Tayakorn000@users.noreply.github.com>
2025-10-24 09:05:31 +0700

    Add files via upload

--- Anti-Finger.sh
+++ Anti-Finger.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-# Create log file
-# touch /home/finger/Anti-Finger/Anti-Finger.log
+cd /home/finger/Anti-Finger || exit
 
 # Activate virtual environment
 source /home/finger/Anti-Finger/venv/bin/activate
 
 # Run the Python script
 python3 /home/finger/Anti-Finger/main.py
+
